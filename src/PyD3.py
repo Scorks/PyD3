54292bc4c5c4dcbeedbe1a763d0fe486a27baee9
Scorks <carostrickland321@gmail.com>
2018-02-28 14:10:17 -0330

    updating PyD3.py

--- src/PyD3.py
+++ src/PyD3.py
@@ -2,6 +2,7 @@
 Implementation of the ID3 algorithm
 '''
 import sys
+import math
 import csv
 import sqlite3
 
@@ -35,17 +36,33 @@ del table_storage[0] # remove the list of just column types
 calculate the initial entropy
 @data: the table_storage
 '''
-def entropy(data):
+def base_entropy(data):
 	class_type_i = " " # first of the two binary class types
 	class_type_ii = " " # second of the two binary class types
+	class_type_i_count = 0 # count for number of binary class I types
+	class_type_ii_count = 0 # count for number of binary class II types
+	sample_size = len(data) # number of cases in the table
+
 	for item in data:
 		if (class_type_i == " "):
 			class_type_i = item[class_type]
 		elif (class_type_i != item[class_type]):
 			class_type_ii = item[class_type]
 			break
-	sample_size = len(table_storage) # number of cases in the table
-	
 
-entropy(table_storage)
+	for item in data:
+		if (item[class_type] == class_type_i):
+			class_type_i_count += 1
+		elif (item[class_type] == class_type_ii):
+			class_type_ii_count += 1
+
+	# calculate the base entropy value
+	equation_section_i = (class_type_i_count/float(sample_size))
+	equation_section_ii = (class_type_ii_count/float(sample_size))
+	entropy = math.log(equation_section_i, 2.0)*(equation_section_i) + math.log(equation_section_ii, 2.0)*(equation_section_ii)
+	
+	return entropy
+
+
+print base_entropy(table_storage)
 
