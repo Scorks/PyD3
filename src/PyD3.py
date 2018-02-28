d5707fd90086aee79806a1bbd2d60bf3b3508d1a
Caro Strickland <carostrickland@Caros-MacBook-Pro.local>
2018-02-28 01:25:16 -0330

    updates to PyD3.py

--- src/PyD3.py
+++ src/PyD3.py
@@ -1,7 +1,24 @@
 '''
 Implementation of the ID3 algorithm
 '''
-
 import sys
+import csv
+import sqlite3
 
-file = open(sys.argv[1], 'r').read().split('\n')
+table_storage = [] # stores a list of dictionaries representing the table
+column_types = [] # stores the names of each column
+
+# resolve the column types
+with open(sys.argv[1]) as f:
+    column_line = f.readline()
+    column_line = column_line.split()
+    column_types = column_line
+
+input_file = open(sys.argv[1], 'r').read().split('\n') # input table
+
+#-----------------------------------------------------------------------------
+
+for line in iter(input_file):
+	line = line.split()
+	if line:
+		# TODO: CREATE LIST OF DICTS FOR TABLE STORAGE
