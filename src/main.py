770ffbf8d4d197fcf18eea19aa58a37e3e8757fd
Caro Strickland <carostrickland321@gmail.com>
2018-04-02 19:49:07 -0230

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,10 +1,7 @@
 from Tkinter import *
 import sys
 import subprocess as sub
-file = './PyD3.py'
-s = sys.argv[1]
-final = file + ' ' + s
-print s
+
 p = sub.Popen(["python", "./PyD3.py"] + sys.argv[1:],stdout=sub.PIPE,stderr=sub.PIPE)
 output, errors = p.communicate()
 
